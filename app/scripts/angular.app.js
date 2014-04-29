0ed3c8e38919fa75e3efc01195b72ad81444fe24
Oliver Stenkilde <oliver@stenkilde.com>
2014-04-29 17:02:24 +0200

    Trying to build Angular

---
+++ app/scripts/angular.app.js
@@ -0,0 +1,73 @@
+// Angular App
+
+	// create the module and name it marvelApp
+	var marvelApp = angular.module('marvelApp', ['ngRoute']);
+
+	// Here we will create our routing
+	marvelApp.config(function($routeProvider, $locationProvider) {
+		$routeProvider
+
+			// Route for home
+			.when('/', {
+				templateUrl: 'pages/home.html',
+				controller:  'mainController'
+			})
+
+			// Route for Heroes
+			.when('/heroes', {
+				templateUrl: 'pages/heroes.html',
+				controller:  'heroesController'
+			})
+
+			// Route for Villains
+			.when('/villains', {
+				templateUrl: 'pages/villains.html',
+				controller:  'villainsController'
+			})	
+
+			// Route for Organizations
+			.when('/organizations', {
+				templateUrl: 'pages/organizations.html',
+				controller:  'organizationController'
+			})	
+
+			// Route for help
+			.when('/help', {
+				templateUrl: 'pages/help.html',
+				controller:  'helpController'
+			})	
+
+			// Route for about
+			.when('/about', {
+				templateUrl: 'pages/about.html',
+				controller:  'aboutController'
+			});
+
+			// use the HTML5 History API
+			$locationProvider.html5Mode(true);
+	});
+
+	// create the controller and inject Angular's $scope
+	marvelApp.controller('mainController', function($scope) {
+		$scope.message = 'So you want to know about Superheroes and the enemies?';
+	});
+
+	marvelApp.controller('heroesController', function($scope) {
+		$scope.message = 'I HERO TEXT';
+	});
+
+	marvelApp.controller('villainsController', function($scope) {
+		$scope.message = 'YOURE A Turd';
+	});
+
+	marvelApp.controller('organizationController', function($scope) {
+		$scope.message = 'YOURE A nurd';
+	});
+
+	marvelApp.controller('helpController', function($scope) {
+		$scope.message = 'YOURE A noob';
+	});
+
+	marvelApp.controller('aboutController', function($scope) {
+		$scope.message = 'YOURE A NERD';
+	});
